15bb0fa792068567b8c481e1163065cd9ad269d9
Qilei Ren <renqilei@ninqiraimatoMacBook-Pro.local>
2014-11-05 23:10:28 +0000

    finish the basic UI design and button connection, without calculating algorithm.

---
+++ Calculator/FormulaCaculation.swift
@@ -0,0 +1,16 @@
+//
+//  FormulaCaculation.swift
+//  Calculator
+//
+//  Created by 任琦磊 on 14/11/5.
+//  Copyright (c) 2014年 任琦磊. All rights reserved.
+//
+
+import Foundation
+
+class FormulaCaculation {
+    
+    func caculating(formulaString: String) {
+        
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var formulaString = ""
+    
+    @IBOutlet var formulaLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func ClickNumberButton(sender: UIButton) {
+        formulaString += sender.currentTitle!
+        formulaLabel.text = formulaString
+    }
+    @IBAction func ClickOperatorButton(sender: UIButton) {
+        formulaString += sender.currentTitle!
+        formulaLabel.text = formulaString
+    }
+    @IBAction func ClickEqualButton(sender: UIButton) {
+        formulaString += sender.currentTitle!
+        formulaLabel.text = formulaString
+        
+        var formulaCaculation: FormulaCaculation!
+        formulaCaculation.caculating(formulaString)
+    }
+    @IBAction func ClickDeleteButton(sender: UIButton) {
+        formulaString = formulaString.substringToIndex(advance(formulaString.startIndex, countElements(formulaString)-1))
+        formulaLabel.text = formulaString
+    }
+    @IBAction func ClickAllDeleteButton(sender: UIButton) {
+        formulaString = ""
+        formulaLabel.text = formulaString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
